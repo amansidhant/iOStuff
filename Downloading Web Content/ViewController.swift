6feff345582b12613a287649cc711c8dd3d71ad7
Aman Sidhant <amansidhant@gmail.com>
2018-01-29 14:19:41 -0800

    part1

--- Downloading Web Content/ViewController.swift
+++ Downloading Web Content/ViewController.swift
@@ -7,19 +7,30 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        
+        webView = WKWebView()
+        
+        let url = URL(string: "https://stackoverflow.com")
+        webView.load(URLRequest(url: url!))
+        webView.allowsBackForwardNavigationGestures = true
+        webView.navigationDelegate = self
+        view = webView
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
